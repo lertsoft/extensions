6a2967936d55c816fbc7f115845e7a609d5e43f8
Ronny Coste <ronny21.97@gmail.com>
2026-01-15 22:19:29 -0500

    fixed a build error

--- extensions/oura/src/utils/ouraData.ts
+++ extensions/oura/src/utils/ouraData.ts
@@ -4,9 +4,9 @@ import { getAccessToken } from "../oauth";
 const API_BASE_URL = "https://api.ouraring.com/v2/";
 
 export function oura<T>(route: string) {
-  const { isLoading, data, revalidate, error } = useCachedPromise(async () => {
+  const { isLoading, data, revalidate, error } = useCachedPromise(async (routeParam: string) => {
     const accessToken = await getAccessToken();
-    const response = await fetch(`${API_BASE_URL}${route}`, {
+    const response = await fetch(`${API_BASE_URL}${routeParam}`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
